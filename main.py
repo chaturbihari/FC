2974913161c1d9eb448018bbf4223d9455ddaa57
TheSilentTeams <shivcollegelife@gmail.com>
2025-07-12 11:55:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from keepAlive import keep_alive
 import asyncio
-from filmy import main as filmy_main
+from filmy import main
 
-keep_alive()  # Starts webserver for Render
-
-async def wrapper():
-    await filmy_main()
-
-# Don't use asyncio.run here!
-loop = asyncio.get_event_loop()
-loop.create_task(wrapper())
-loop.run_forever()
+keep_alive()
+asyncio.run(main())
