ad8cd581f1410718d64baccfeae672a835131d02
TheSilentTeams <shivcollegelife@gmail.com>
2025-07-13 11:14:43 +0530

    Update keepAlive.py

--- keepAlive.py
+++ keepAlive.py
@@ -19,7 +19,7 @@ def run():
 def ping_self():
     while True:
         try:
-            url = "https://test-ujwm.onrender.com"
+            url = "https://fc-cbhg.onrender.com"
             requests.get(url)
             print("✅ Self-ping sent")
         except Exception as e:
